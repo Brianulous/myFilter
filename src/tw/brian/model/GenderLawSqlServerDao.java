07794ce4a6dd289620096cfe4930c3b72e70b787
brian <thisisbianchang@gmail.com>
2023-03-28 22:50:06 +0800

    Revert "Reformat"
    This reverts commit c5ebd068e4c3db244afb9f9d713db08c63033ce9.

--- src/tw/brian/model/GenderLawSqlServerDao.java
+++ src/tw/brian/model/GenderLawSqlServerDao.java
@@ -8,6 +8,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+
+import tw.brian.exception.GenderSQLException;
 import tw.brian.exception.LaborSQLException;
 import tw.brian.util.LocalDateutil;
 
@@ -62,7 +64,7 @@ public class GenderLawSqlServerDao implements GenderLawDao {
 		preState.setString(2, enterprise);
 		ResultSet rs = preState.executeQuery();
 		if (!rs.next()) {
-			throw new SQLException("查無相關性平法案件資料");
+			throw new GenderSQLException("查無相關性平法案件資料");
 		}
 		GenderLawCase genderLawCase = new GenderLawCase(rs.getInt("id"),
 														rs.getDate("punish_date"),
@@ -86,7 +88,7 @@ public class GenderLawSqlServerDao implements GenderLawDao {
 		preState.setString(2, enterprise);
 		ResultSet rs = preState.executeQuery();
 		if (!rs.next()) {
-			throw new SQLException("查無相關性平法案件資料");
+			throw new GenderSQLException("查無相關性平法案件資料");
 		}
 		GenderLawCase genderLawCase = new GenderLawCase(rs.getInt("id"),
 														rs.getDate("punish_date"),
@@ -171,7 +173,7 @@ public class GenderLawSqlServerDao implements GenderLawDao {
 			lawCases.add(genderLawCase);
 		}
 		if (lawCases.isEmpty()) {
-			throw new SQLException("查無相關日期資料");
+			throw new GenderSQLException("查無相關日期資料");
 		}
 
 		return lawCases;
@@ -200,7 +202,7 @@ public class GenderLawSqlServerDao implements GenderLawDao {
 			lawCases.add(genderLawCase);
 		}
 		if (lawCases.isEmpty()) {
-			throw new SQLException("查無相關日期資料");
+			throw new GenderSQLException("查無相關日期資料");
 		}
 
 		return lawCases;
@@ -231,7 +233,7 @@ public class GenderLawSqlServerDao implements GenderLawDao {
 			lawCases.add(genderLawCase);
 		}
 		if (lawCases.isEmpty()) {
-			throw new SQLException("查無相關日期資料");
+			throw new GenderSQLException("查無相關日期資料");
 		}
 
 		return lawCases;
@@ -258,7 +260,7 @@ public class GenderLawSqlServerDao implements GenderLawDao {
 			lawCases.add(genderLawCase);
 		}
 		if (lawCases.isEmpty()) {
-			throw new SQLException("查無相關日期資料");
+			throw new GenderSQLException("查無相關日期資料");
 		}
 
 		return lawCases;
